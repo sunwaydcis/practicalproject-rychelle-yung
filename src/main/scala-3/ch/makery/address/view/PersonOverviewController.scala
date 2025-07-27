64c8b28de587e9b749cb45a6ad3147ed6d1107d2
rychelle-yung <rychelle.yung@gmail.com>
2025-07-27 18:09:48 +0800

    Part 5

--- src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
+++ src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
@@ -74,7 +74,7 @@ class PersonOverviewController():
   def handleDeletePerson(action: ActionEvent) =
     val selectedIndex = personTable.selectionModel().selectedIndex.value
     if (selectedIndex >= 0) then
-      personTable.items().remove(selectedIndex)
+      personTable.items().remove(selectedIndex).delete()
     else
       // Nothing selected.
       val alert = new Alert(AlertType.Error):
@@ -88,8 +88,10 @@ class PersonOverviewController():
   def handleNewPerson(action: ActionEvent) =
     val person = new Person("", "")
     val okClicked = MainApp.showPersonEditDialog(person);
-    if (okClicked) then
+    if (okClicked) then {
       MainApp.personData += person
+      person.save()
+    }
 
   @FXML
   def handleEditPerson(action: ActionEvent) =
@@ -97,16 +99,18 @@ class PersonOverviewController():
     if (selectedPerson != null) then
       val okClicked = MainApp.showPersonEditDialog(selectedPerson)
 
-      if (okClicked) then showPersonDetails(Some(selectedPerson))
+      if (okClicked) then {
+        showPersonDetails(Some(selectedPerson))
+        selectedPerson.save()
 
-    else
-      // Nothing selected.
-      val alert = new Alert(Alert.AlertType.Warning):
-        initOwner(MainApp.stage)
-        title = "No Selection"
-        headerText = "No Person Selected"
-        contentText = "Please select a person in the table."
-      .showAndWait()
+      } else
+        // Nothing selected.
+        val alert = new Alert(Alert.AlertType.Warning):
+          initOwner(MainApp.stage)
+          title = "No Selection"
+          headerText = "No Person Selected"
+          contentText = "Please select a person in the table."
+        .showAndWait()
 
 
 
--- src/main/scala-3/ch/makery/address/view/RootLayoutController.scala
+++ src/main/scala-3/ch/makery/address/view/RootLayoutController.scala
@@ -6,7 +6,11 @@ import javafx.fxml.FXML
 
 @FXML
 class RootLayoutController():
-  
+
   @FXML
   def handleClose(action: ActionEvent): Unit =
     MainApp.stage.close()
+
+  @FXML
+  def handleDelete(action: ActionEvent): Unit =
+    MainApp.personOverviewController.map(x => x.handleDeletePerson(action))
